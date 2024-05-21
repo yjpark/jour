f393cd192aa543fb2a20ab81af47b96175633e98
YJ Park <yjpark@gmail.com>
2024-05-21 23:35:17 +0800

    🚧 [WIP] - 2024-05-21 23:35:17 -

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-    schema: "./db/schema/*",
+    schema: "./db/schema",
     out: "./db/migrations",
     dialect: "mysql",
     dbCredentials: {
